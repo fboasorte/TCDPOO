fee0d2a20d4482cf51617c570c890ab30e7f1411
Felipe Boa Sorte <feliperocha.boasorte@gmail.com>
2020-12-09 10:36:14 -0300

    Adicionado Equals/HashCode em Disciplina.java

--- src/main/java/br/edu/ifnmg/poo/trabalhofinalpoo/entity/Disciplina.java
+++ src/main/java/br/edu/ifnmg/poo/trabalhofinalpoo/entity/Disciplina.java
@@ -5,6 +5,8 @@
  */
 package br.edu.ifnmg.poo.trabalhofinalpoo.entity;
 
+import java.util.Objects;
+
 /**
  *
  * @author felipe
@@ -15,6 +17,7 @@ public class Disciplina extends Entidade {
 
     private String conteudo;
 
+    //<editor-fold defaultstate="collapsed" desc="Construtores">
     public Disciplina() {
     }
 
@@ -24,6 +27,8 @@ public class Disciplina extends Entidade {
         this.conteudo = conteudo;
     }
 
+//</editor-fold>
+    
     //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
     public String getNome() {
         return nome;
@@ -42,6 +47,38 @@ public class Disciplina extends Entidade {
     }
 
 //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc="Equals/HashCode">
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.nome);
+        hash = 79 * hash + Objects.hashCode(this.conteudo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Disciplina other = (Disciplina) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.conteudo, other.conteudo)) {
+            return false;
+        }
+        return true;
+    }
+//</editor-fold>
+
     @Override
     public String toString() {
         return "Disciplina{" + "nome=" + nome + ", conteudo=" + conteudo + '}';
