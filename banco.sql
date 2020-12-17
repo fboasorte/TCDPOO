fe218b474240a692f7f98263d04a306ec1acb9b0
Felipe Boa Sorte <feliperocha.boasorte@gmail.com>
2020-12-17 11:42:03 -0300

    Mudancao final para o bando de bados

--- banco.sql
+++ banco.sql
@@ -13,21 +13,27 @@ cpf int,
 nome varchar(50),
 nascimento varchar(15));
 
+create table disponibilidadeHorario (
+id int primary key auto_increment NOT NULL,
+data date,
+hora varchar(5));
+
 create table disciplina (
 id int primary key auto_increment NOT NULL,
 nome varchar(20),
-conteudo varchar(100));
+conteudo varchar(100),
+idDisponibilidadeHorario int,
+foreign key (idDisponibilidadeHorario) references disponibilidadeHorario(id));
 
 create table avaliacao (
 id int primary key auto_increment NOT NULL,
 notaParteEscrita int,
 notaParteOral char,
-comentario varchar(100));
-
-create table disponibilidadeHorario (
-id int primary key auto_increment NOT NULL,
-data date,
-hora varchar(5));
+comentario varchar(100),
+idDiscente int,
+idDisciplina int,
+foreign key (idDiscente) references discente(id),
+foreign key (idDisciplina) references disciplina(id));
 
 create table matricula (
 id int primary key auto_increment NOT NULL,
